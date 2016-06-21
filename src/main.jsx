3bc8e84f6ee05c9dd617c5c39c70ee1461c7207c
Juan Ferreira <juanferreira@Juans-iMac.home>
2016-06-21 19:50:37 -0400

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+var React = require('react'),
+	ReactDOM = require('react-dom'),
+	ListManager = require('./components/ListManager.jsx');
+
+ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
